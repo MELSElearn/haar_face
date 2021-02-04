b4697c180b1f5875ced5bc61389189f884eada4f
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2021-02-04 14:16:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,30 @@ def catch_frame(data):
     ## send it back to client
     emit('response_back', data)  ## ??
 
+@socketio.on('image')
+def image(data_image):
+    sbuf = StringIO()
+    sbuf.write(data_image)
 
+    # decode and convert into image
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+
+    ## converting RGB to BGR, as opencv standards
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+    # Process the image frame
+    frame = imutils.resize(frame, width=700)
+    frame = cv2.flip(frame, 1)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+
+    # base64 encode
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpg;base64,'
+    stringData = b64_src + stringData
+
+    # emit the frame back
+    emit('response_back', stringData)
+    
 if __name__ == '__main__':
     socketio.run(app)
